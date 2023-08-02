7d5f3bda86ed876dabcbd933db1febd0ed01f25d
Madhu sudan Bhattarai <summerpalpali@gmail.com>
2023-08-02 16:01:13 +0545

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Real_Time_Chat.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,24 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    li.textContent = `${user}: ${message}`;
+    document.getElementById("messages").appendChild(li);
+});
+
+document.getElementById("sendButton").addEventListener("click", function () {
+    var user = "User"; // Replace this with the actual username
+    var message = document.getElementById("userInput").value;
+
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        console.error(err.toString());
+    });
+
+    document.getElementById("userInput").value = "";
+});
+
+connection.start().then(function () {
+    console.log("Connected to the chat hub.");
+}).catch(function (err) {
+    console.error(err.toString());
+});
